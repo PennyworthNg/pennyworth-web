ea103a86d06918f4dda5a59d1a89ecb36ef7d17f
Emmy <emmyconceptng@gmail.com>
2023-10-06 19:57:37 +0100

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pennyworth</title>
+    <link rel="icon" type="image/x-icon" href="assets/logo/logo-single.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
@@ -17,7 +17,7 @@
 <body>
     <div id="main"></div>
     @viteReactRefresh
-    @vite('resources/js/src/index.jsx')
+    @vite(['resources/sass/app.scss', 'resources/js/src/index.jsx'])
 
 
 </body>
